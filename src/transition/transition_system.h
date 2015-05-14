591c4c54d72925a33affa75cc101b54ea8e8c8b4
Milan Straka <fox@ucw.cz>
2015-05-14 22:59:04 +0200

    Add abstract transition_system class.

---
+++ src/transition/transition_system.h
@@ -0,0 +1,32 @@
+// This file is part of Parsito <http://github.com/ufal/parsito/>.
+//
+// Copyright 2015 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "common.h"
+#include "transition.h"
+
+namespace ufal {
+namespace parsito {
+
+class transition_system {
+ public:
+  virtual ~transition_system() {}
+
+  virtual unsigned transitions() const = 0;
+  virtual void init(configuration& c, tree& t, const tree* golden = nullptr) = 0;
+  virtual void perform(unsigned transition) = 0;
+  virtual void losses(vector<int>& losses) = 0;
+
+  virtual void create(const vector<string>& labels) = 0;
+  static transition_system* create(const string& name);
+};
+
+} // namespace parsito
+} // namespace ufal
