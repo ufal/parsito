dfa945c17be501b91fe1bb2545f796e33e122fe9
Milan Straka <fox@ucw.cz>
2015-05-26 21:49:47 +0200

    Compute real logprob, not only "prob".

--- src/parser/parser_nn_trainer.cpp
+++ src/parser/parser_nn_trainer.cpp
@@ -247,7 +247,10 @@ void parser_nn_trainer::train(const string& transition_system_name, const string
 
           // Apply the oracle
           auto prediction = oracle->predict(conf, gold, network_best);
-          logprob += workspace.outcomes[prediction.best];
+
+          // Update logprob
+          if (workspace.outcomes[prediction.best])
+            logprob += log(workspace.outcomes[prediction.best]);
 
           // Backpropagate the chosen outcome
           network_trainer.backpropagate(parser.embeddings, extracted_embeddings, prediction.best, workspace);
